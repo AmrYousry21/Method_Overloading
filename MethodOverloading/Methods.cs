ea3d220eb85d5add87634e988cb28941f17a4a12
Amr Yousry <amryousry19@gmail.com>
2022-08-15 15:37:51 -0400

    Method Overloading Exercise

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class Methods
+    {
+        public int Add (int numA, int numB) 
+        {
+            return numA + numB;
+        }
+
+        public double Add (double numA, double numB) 
+        {
+            return (numA + numB);
+        }
+
+        public string Add (int numA, int numB, bool condition) 
+        {
+            string result = "";
+
+            if (condition) 
+            {
+                int sum = numA + numB;
+
+                if (sum == 1)
+                {
+                    result = sum.ToString() + " Dollar";
+                }
+
+                else 
+                {
+                    result = sum.ToString() + " Dollars";
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MethodOverloading // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods program = new Methods();
+
+            Console.WriteLine(program.Add(5, 2));
+            Console.WriteLine(program.Add(6.7, 3.5));
+            Console.WriteLine(program.Add(0, 1, true));
+        }
+    }
+}
